781e6b42213685f352b3a62742f928156ff901cd
Alexandru <alexandru.varatic@student.upt.ro>
2022-05-22 13:30:18 +0300

    SimDiscretN

--- distributii/discrete/SimDiscretN.m
+++ distributii/discrete/SimDiscretN.m
@@ -1,8 +1,26 @@
 % sim. var. aleat. discreta de distrib. generala neuniforma
 function xk = SimDiscretN(x, p, n)	% x, p = tablouri
+
+    xk = [];
+
+    % verificarea conditiilor
+    for i = 1 : numel(x)-1
+        if x(i) > x(i+1)
+            printf("Eroare! Nu se respecta ordinea crescatoare a valorilor lui x !\n");
+            return;
+        end
+    end
+    
+    if sum(p) ~= 1
+        printf("Eroare! Suma probabilitatilor nu este 1 !\n");
+        return;
+    end
+
+    
 	k = 1;
 	F = p(1);
 	u = rand;
+
 	while u >= F
         F = F + p(k);
 		k = k + 1;
